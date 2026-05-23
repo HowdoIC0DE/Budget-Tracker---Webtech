40c26e7ab0bcf7caf419e6dc902e653235c8a08d
HowdoIC0DE <137148316+HowdoIC0DE@users.noreply.github.com>
2026-05-23 13:48:19 +0200

    import updated @CrossOrigin

--- src/main/java/com/example/budgettracker/controller/TransactionController.java
+++ src/main/java/com/example/budgettracker/controller/TransactionController.java
@@ -3,7 +3,9 @@ import com.example.budgettracker.model.Transaction;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class TransactionController {
 
