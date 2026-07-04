1699a8dc3da7b352c51ea251d3aec919c348bc14
HowdoIC0DE <137148316+HowdoIC0DE@users.noreply.github.com>
2026-07-04 23:04:33 +0200

    added test

--- src/test/java/com/example/budgettracker/BudgettrackerApplicationTests.java
+++ src/test/java/com/example/budgettracker/BudgettrackerApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.budgettracker;
 
+import com.example.budgettracker.model.Transaction;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class BudgettrackerApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+	void transactionCanBeCreated() {
+		Transaction transaction = new Transaction(
+				null,
+				"Miete",
+				650.0,
+				"EXPENSE",
+				"2026-06-17"
+		);
 
+		assertEquals("Miete", transaction.getTitle());
+		assertEquals(650.0, transaction.getAmount());
+		assertEquals("EXPENSE", transaction.getType());
+		assertEquals("2026-06-17", transaction.getDate());
+	}
 }
